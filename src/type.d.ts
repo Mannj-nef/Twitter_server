55199ecc61b75461d9acda892b8383303aedfe14
Manh Quan <manhquan.05012002@gmail.com>
2023-11-07 08:39:02 +0700

    update code

--- src/type.d.ts
+++ src/type.d.ts
@@ -2,10 +2,12 @@ import { Request } from 'express';
 import { Jwt } from 'jsonwebtoken';
 import { UserModel } from './models/schemas';
 import { TokenPayload } from './interfaces/requests';
+import TweetModel from './models/schemas/Tweet';
 
 declare module 'express' {
   interface Request {
     user?: UserModel;
+    tweet?: TweetModel;
     decoded_token?: TokenPayload;
   }
 }
